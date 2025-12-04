95338be88b3414f10423064e785c0997f04c9982
Maxime Delorme <maxime@familledelorme.com>
2025-12-04 17:42:26 +0100

    Fix tab bars with correct style and icons and title

--- app/(main)/_layout.js
+++ app/(main)/_layout.js
@@ -3,6 +3,7 @@ import colors from "../../assets/css/colors";
 import { Logo } from "../../components";
 import { Text } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+
 export default function Layout() {
   return (
     <Tabs
@@ -16,21 +17,35 @@ export default function Layout() {
     >
       <Tabs.Screen
         options={{
-          tabBarLabel: ({ focused, color }) => (
-            <Text style={{ color: focused ? colors.primary : color }}>
-              Home
-            </Text>
-          ),
+          tabBarLabel: "Home",
+          tabBarActiveTintColor: colors.primary,
           tabBarIcon: ({ focused, color, size }) => (
-            <FontAwesome
-              name="home"
-              size={size}
-              color={focused ? colors.primary : color}
-            />
+            <FontAwesome name="home" size={size} color={color} />
           ),
         }}
         name="home"
-      ></Tabs.Screen>
+      />
+
+      <Tabs.Screen
+        name="map"
+        options={{
+          tabBarLabel: "Map",
+          tabBarActiveTintColor: colors.primary,
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome name="map-marker" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarActiveTintColor: colors.primary,
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome name="user-o" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
